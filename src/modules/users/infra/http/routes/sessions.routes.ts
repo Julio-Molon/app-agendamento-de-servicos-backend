4761c54130b23b080a66c2a103ab1c6de5199a4e
Júlio <juliomolon@live.com>
2020-09-06 00:51:00 -0300

    Configurando imports absolutos e restruturando projeto com DDD e SOLID

---
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-shadow */
+import { Router } from 'express';
+import SessionsController from '../controllers/SessionsController';
+
+const sessionsRouter = Router();
+const sessionsController = new SessionsController();
+
+sessionsRouter.post('/', sessionsController.create);
+
+export default sessionsRouter;
