13d069260408173409b2b06caea0b58a78c02050
Júlio <juliomolon@live.com>
2020-12-22 22:59:23 -0300

    Agenda do prestador, configurando mongo, estrutura e envio de notificações, refatorando teste

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,4 +1,4 @@
-import { createConnection } from 'typeorm';
+import { createConnections } from 'typeorm';
 
 /* le as credencias de conexção do ormconfig.json */
-createConnection();
+createConnections();
